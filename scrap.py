b1ec077b4b4905d8fd5e10f52c7377dee8b5884d
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-12-02 12:21:55 +0700

    scrap kecamatan options

--- scrap.py
+++ scrap.py
@@ -4,18 +4,38 @@ import requests
 from bs4 import BeautifulSoup
 
 HOME_URL = 'http://sekolah.data.kemdikbud.go.id/'
+KEC_URL = HOME_URL + 'chome/kecamatan/'
 
 home_page = requests.get(HOME_URL)
 soup = BeautifulSoup(home_page.content, 'html.parser')
-
 kota_options = soup.find(id='kode_kabupaten').find_all('option')
 
 count = 0
 for option in kota_options:
     text = option.text
     value = option['value']
+    if not value:
+        continue
+
     print(value + ' ' + text)
-    
+    kec_page = requests.post(KEC_URL,
+        data={'kode_kabupaten': str(value)})
+    kec_soup = BeautifulSoup(kec_page.content, 'html.parser')
+    kec_options = kec_soup.find(id='kode_kec').find_all('option')
+
+    kec_count = 0
+    for kec_option in kec_options:
+        kec_text = kec_option.text
+        kec_value = kec_option['value']
+        if not kec_value:
+            continue
+
+        print('    ' + kec_value + ' ' + kec_text)
+
+        kec_count += 1
+        if kec_count == 10:
+            break
+
     count += 1
     if count == 10:
         break
