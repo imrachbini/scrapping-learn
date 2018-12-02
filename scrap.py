fe621d20845ead4fbddcfde8df7566776f660940
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-12-02 11:37:28 +0700

    extract kecamatan options value

--- scrap.py
+++ scrap.py
@@ -8,9 +8,14 @@ HOME_URL = 'http://sekolah.data.kemdikbud.go.id/'
 home_page = requests.get(HOME_URL)
 soup = BeautifulSoup(home_page.content, 'html.parser')
 
-kota_options = soup.find(id='kode_kabupaten')
+kota_options = soup.find(id='kode_kabupaten').find_all('option')
 
+count = 0
 for option in kota_options:
     text = option.text
     value = option['value']
     print(value + ' ' + text)
+    
+    count += 1
+    if count == 10:
+        break
